46a33ec5096c72aa92960a721770bdbc44dcbe0d
Yoga <LastRoze@users.noreply.github.com>
2020-05-08 16:16:00 +0700

    Create ZUZU-Browser.meta.js

---
+++ ZUZU-Browser.meta.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         ZUZU Browser
+// @namespace    ZUZU Browser
+// @description  Make your browser BLAZING FAST !!!
+// @copyright    ©2020 - Yoga Budiman
+// @version      1.0
+// @author       Last Roze
+// @homepageURL  https://github.com/LastRoze/
+// @supportURL   https://linkedin.com/in/lastroze
+// @updateURL    https://github.com/LastRoze/ZUZU-Browser/raw/master/ZUZU-Browser.meta.js
+// @downloadURL  https://github.com/LastRoze/ZUZU-Browser/raw/master/ZUZU-Browser.user.js
+// @icon         https://github.com/LastRoze/ZUZU-Browser/blob/master/ZUZU%20Hospitality.jpg?raw=true
+// @require      https://code.jquery.com/jquery-latest.js
+// @run-at       document-start
+// @include      *
+// @include      http://*
+// @include      https://*
+// @match        *://*/*
+// @connect      *
+// @grant        GM_addStyle
+// @grant        GM_deleteValue
+// @grant        GM_getResourceURL
+// @grant        GM_getValue
+// @grant        GM_openInTab
+// @grant        GM_registerMenuCommand
+// @grant        GM_setValue
+// @grant        GM_xmlhttpRequest
+// @grant        GM.deleteValue
+// @grant        GM.getResourceUrl
+// @grant        GM.getValue
+// @grant        GM.openInTab
+// @grant        GM.xmlHttpRequest
+// @grant        unsafeWindow
+// @noframes
+// ==/UserScript==

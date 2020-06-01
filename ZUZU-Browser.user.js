f7dd3227cb7c8118e658c5dc723212260edd1492
Yoga <LastRoze@users.noreply.github.com>
2020-06-01 13:51:13 +0700

    Update ZUZU-Browser.user.js

--- ZUZU-Browser.user.js
+++ ZUZU-Browser.user.js
@@ -3,7 +3,7 @@
 // @namespace    ZUZU Browser
 // @description  Make your browser BLAZING FAST !!!
 // @copyright    ©2020 - Yoga Budiman
-// @version      2.0
+// @version      2.1
 // @author       Last Roze
 // @homepageURL  https://github.com/LastRoze/
 // @supportURL   https://linkedin.com/in/lastroze

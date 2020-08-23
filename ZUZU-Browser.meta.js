283397db8d9572ba39fca1a6cc942f6b2bb6a8b9
Yoga Budiman <LastRoze@users.noreply.github.com>
2020-08-23 18:22:33 +0700

    Update ZUZU-Browser.meta.js

--- ZUZU-Browser.meta.js
+++ ZUZU-Browser.meta.js
@@ -1,34 +1,42 @@
 // ==UserScript==
-// @name         ZUZU Browser
-// @namespace    ZUZU Browser
-// @description  Make your browser BLAZING FAST !!!
-// @copyright    ©2020 - Yoga Budiman
-// @version      5.1
-// @author       Last Roze
-// @homepageURL  https://github.com/LastRoze/
-// @supportURL   https://linkedin.com/in/lastroze
-// @updateURL    https://github.com/LastRoze/ZUZU-Browser/raw/master/ZUZU-Browser.meta.js
-// @downloadURL  https://github.com/LastRoze/ZUZU-Browser/raw/master/ZUZU-Browser.user.js
-// @icon         https://github.com/LastRoze/ZUZU-Browser/blob/master/ZUZU%20Hospitality.jpg?raw=true
-// @run-at       document-start
-// @include      *
-// @include      http://*
-// @include      https://*
-// @match        *://*/*
-// @connect      *
-// @grant        GM_addStyle
-// @grant        GM_deleteValue
-// @grant        GM_getResourceURL
-// @grant        GM_getValue
-// @grant        GM_openInTab
-// @grant        GM_registerMenuCommand
-// @grant        GM_setValue
-// @grant        GM_xmlhttpRequest
-// @grant        GM.deleteValue
-// @grant        GM.getResourceUrl
-// @grant        GM.getValue
-// @grant        GM.openInTab
-// @grant        GM.xmlHttpRequest
-// @grant        unsafeWindow
-// @noframes
+// @name        ZUZU Browser
+// @namespace   ZUZU Browser
+// @version		5.2
+// @author		Last Roze
+// @description	Make your browser BLAZING FAST!
+// @copyright	©2020 - Yoga Budiman
+// @homepage	https://github.com/LastRoze/
+// @homepageURL	https://github.com/LastRoze/
+// @website		https://lastroze.github.io/
+// @source		https://github.com/LastRoze/DNA-Shield
+// @icon		https://github.com/LastRoze/ZUZU-Browser/blob/master/ZUZU%20Hospitality.jpg?raw=true
+// @iconURL		https://github.com/LastRoze/ZUZU-Browser/blob/master/ZUZU%20Hospitality.jpg?raw=true
+// @defaulticon	https://github.com/LastRoze/ZUZU-Browser/blob/master/ZUZU%20Hospitality.jpg?raw=true
+// @icon64		https://github.com/LastRoze/ZUZU-Browser/blob/master/ZUZU%20Hospitality.jpg?raw=true
+// @icon64URL	https://github.com/LastRoze/ZUZU-Browser/blob/master/ZUZU%20Hospitality.jpg?raw=true
+// @updateURL   https://github.com/LastRoze/ZUZU-Browser/raw/master/ZUZU-Browser.meta.js
+// @downloadURL https://github.com/LastRoze/ZUZU-Browser/raw/master/ZUZU-Browser.user.js
+// @supportURL	https://lastroze.github.io/
+// @include		*
+// @exclude		https://docs.google.com/*
+// @connect		*
+// @run-at		document-start
+// @grant		GM_addStyle
+// @grant		GM_deleteValue
+// @grant		GM_download
+// @grant		GM_getResourceText
+// @grant		GM_getResourceURL
+// @grant		GM_getValue
+// @grant		GM_listValues
+// @grant		GM_notification
+// @grant		GM_openInTab
+// @grant		GM_registerMenuCommand
+// @grant		GM_setClipboard
+// @grant		GM_setValue
+// @grant		GM_unregisterMenuCommand
+// @grant		GM_xmlhttpRequest
+// @grant		unsafeWindow
+// @grant		window.close
+// @grant		window.focus
+// @grant		window.onurlchange
 // ==/UserScript==
